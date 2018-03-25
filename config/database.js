e3617e2af5c76be0ae80396bf86b6339627772aa
Sherman Lye <cheeming0627@hotmail.com>
2018-03-25 20:42:28 +0800

    Revert "Initial commit"
    This reverts commit 5d3b8dfefe106d9c428858035b95db8a4f759108.

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-    
-        'url' : 'mongodb://admin:admin@ds153179.mlab.com:53179/fatetest' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    
-    };
