ddf0e6c0b974650334427aa00944666dc6b8afaf
Sherman Lye <cheeming0627@hotmail.com>
2018-03-25 20:58:07 +0800

    Re-Intial Commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+    
+        'url' : 'mongodb://admin:admin@ds153179.mlab.com:53179/fatetest' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    
+    };
