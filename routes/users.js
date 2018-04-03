182c2d2cdfbd7674994da96e7fe19542f2220440
Sherman Lye <cheeming0627@hotmail.com>
2018-04-03 22:48:15 +0800

    Added connect link but unlink not working

--- routes/users.js
+++ routes/users.js
@@ -5,82 +5,134 @@ var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
 
-// Register
-router.get('/register', function(req, res){
-	res.render('register.handlebars');
-});
+// =============================================================================
+// normal routes ==================================================
+// =============================================================================
 
-// Login
-router.get('/login', function(req, res){
-	if(req.user) {
-		res.render('index.handlebars');
-	}else{
-		res.render('login.ejs', { message: req.flash('loginMessage') });
-	}
-});
+	// HOME =========================
+	router.get('/', function(req, res){
+		res.render('index.ejs');
+	});
 
-// Login
-router.get('/', function(req, res){
-	res.render('index.ejs');
-});
+	// PROFILE SECTION =========================
+	router.get('/profile', ensureAuthenticated, function(req, res){
+		res.render('profile.ejs', {
+			user : req.user
+		}); // if it's logon then go to this
+	});
 
-// Profile
-router.get('/profile', ensureAuthenticated, function(req, res){
-	res.render('index.handlebars', {
-		user : req.user
-	}); // if it's logon then go to this
-});
+	// LOGOUT ==============================
+	router.get('/logout', function(req, res){
+		req.logout();
+	
+		req.flash('success_msg', 'You are logged out');
+	
+		res.redirect('/');
+	});
 
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		//req.flash('error_msg','You are not logged in');
-		//res.redirect('/users/login'); //SHERMAN : 1ST DEFAULT PAGE
-		res.redirect('/'); //SHERMAN : 1ST DEFAULT PAGE
-	}
-}
+// =============================================================================
+// AUTHENTICATE (FIRST LOGIN) ==================================================
+// =============================================================================
 
-// Register User
-router.post('/register', function(req, res){
-	var name = req.body.name;
-	var email = req.body.email;
-	var username = req.body.username;
-	var password = req.body.password;
-	var password2 = req.body.password2;
-
-	// Validation
-	req.checkBody('name', 'Name is required').notEmpty();
-	req.checkBody('email', 'Email is required').notEmpty();
-	req.checkBody('email', 'Email is not valid').isEmail();
-	req.checkBody('username', 'Username is required').notEmpty();
-	req.checkBody('password', 'Password is required').notEmpty();
-	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
-
-	var errors = req.validationErrors();
-
-	if(errors){
-		res.render('register.handlebars',{
-			errors:errors
-		});
-	} else {
-		var newUser = new User({
-			'local.username': username,
-			'local.email':email,
-			'local.name': name,
-			'local.password': password
+	// locally --------------------------------
+		// LOGIN ===============================
+		// show the login form
+		router.get('/login', function(req, res){
+			if(req.user) {
+				res.render('profile.ejs');
+			}else{
+				res.render('login.ejs', { message: req.flash('loginMessage') });
+			}
 		});
 
-		User.createUser(newUser, function(err, user){
-			if(err) throw err;
-			console.log(User);
+		// process the login form	
+		router.post('/login',passport.authenticate('local-login', {
+			successRedirect:'/profile', 
+			failureRedirect:'/login',
+			failureFlash: true
+		}));
+
+		// SIGNUP =================================
+		// show the signup form
+		router.get('/register', function(req, res){
+			res.render('register.ejs', { message: req.flash('loginMessage') });
 		});
 
-		req.flash('success_msg', 'You are registered and can now login');
+		// process the signup form
+		router.post('/register', function(req, res){
+			var name = req.body.name;
+			var email = req.body.email;
+			var username = req.body.username;
+			var password = req.body.password;
+			var password2 = req.body.password2;
 
-		res.redirect('/users/login');
-	}
-});
+			// Validation
+			req.checkBody('name', 'Name is required').notEmpty();
+			req.checkBody('email', 'Email is required').notEmpty();
+			req.checkBody('email', 'Email is not valid').isEmail();
+			req.checkBody('username', 'Username is required').notEmpty();
+			req.checkBody('password', 'Password is required').notEmpty();
+			req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+			var errors = req.validationErrors();
+
+			if(errors){
+				res.render('register.ejs',{
+					errors:errors
+				});
+			} else {
+				var newUser = new User({
+					'local.username': username,
+					'local.email':email,
+					'local.name': name,
+					'local.password': password
+				});
+
+				User.createUser(newUser, function(err, user){
+					if(err) throw err;
+					console.log(User);
+				});
+
+				req.flash('success_msg', 'You are registered and can now login');
+
+				res.redirect('/login');
+			}
+		});
+
+// =============================================================================
+// AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
+// =============================================================================
+
+	// locally --------------------------------
+		router.get('/connect/local', function(req, res) {
+			res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+		});
+		router.post('/connect/local', passport.authenticate('local-signup', {
+			successRedirect : '/profile', // redirect to the secure profile section
+			failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
+			failureFlash : true // allow flash messages
+		}));
+
+
+
+// =============================================================================
+// UNLINK ACCOUNTS =============================================================
+// =============================================================================
+// used to unlink accounts. for social accounts, just remove the token
+// for local account, remove email and password
+// user account will stay active in case they want to reconnect in the future
+
+	// local -----------------------------------
+		router.get('/unlink/local', function(req, res) {
+			var user            = req.user;
+			user.local.email    = undefined;
+			user.local.password = undefined;
+			user.save(function(err) {
+				res.redirect('/profile');
+			});
+		});
+
+
 
 passport.use('local-login', new LocalStrategy({ 
 		passReqToCallback: true 
@@ -105,8 +157,9 @@ passport.use('local-login', new LocalStrategy({
 			});
 			});
 		});
-  }));
-
+	}));
+	
+	
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
@@ -117,18 +170,13 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
-router.post('/login',passport.authenticate('local-login', {
-	successRedirect:'/profile', 
-	failureRedirect:'/login',
-	failureFlash: true
-}));
-
-router.get('/logout', function(req, res){
-	req.logout();
-
-	req.flash('success_msg', 'You are logged out');
-
-	res.redirect('/');
-});
-
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		//res.redirect('/users/login'); //SHERMAN : 1ST DEFAULT PAGE
+		res.redirect('/'); //SHERMAN : 1ST DEFAULT PAGE
+	}
+}
 module.exports = router;
